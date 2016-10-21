6644a8f88e1038b136634d031da882800c2bbdc9
Unilks <tank357@icloud.com>
2016-10-22 02:06:33 +0800

    now we can support the property of python

--- serializer.py
+++ serializer.py
@@ -1,40 +1,41 @@
-def serializer(origin_class):
+def serializer(origin_instance):
 
-    def cycling(obj):
-        if isinstance(obj, (set, list)):
+    def cycling(instance):
+        if isinstance(instance, (set, list)):
             m_list = []
-            for item in obj:
+            for item in instance:
                 value = typping(item)
                 m_list.append(value)
             return m_list
-        if isinstance(obj, dict):
+        if isinstance(instance, dict):
             m_dict = {}
-            for item in obj:
-                value = typping(obj[item])
+            for item in instance:
+                value = typping(instance[item])
                 m_dict.update({item: value})
             return m_dict
 
-    def typping(obj):
+    def typping(instance):
 
-        if isinstance(obj, set):
-            return cycling(obj)
-        elif isinstance(obj, list):
-            return cycling(obj)
-        elif isinstance(obj, dict):
-            return cycling(obj)
-        elif isinstance(obj, (float, int, basestring, bool)):
-            return obj
+        if isinstance(instance, set):
+            return cycling(instance)
+        elif isinstance(instance, list):
+            return cycling(instance)
+        elif isinstance(instance, dict):
+            return cycling(instance)
+        elif isinstance(instance, (float, int, basestring, bool)):
+            return instance
         else:
-            return typping(mapping(obj))
+            return typping(mapping(instance))
 
-    def mapping(obj):
-        return attr_dict(obj)
+    def mapping(instance):
+        return attr_dict(instance)
 
-    def attr_dict(obj):
-        full = dict([[e, obj.__dict__[e]]
-             for e in obj.__dict__ if '__' not in e and not hasattr(obj.__dict__[e], '__call__')])
-        proper = dict([[p, obj.__dict__[p].__get__(obj, type(obj))]
-               for p in full if hasattr(full[p], 'fset')])
+    def attr_dict(instance):
+
+        full = dict([[e, getattr(instance, e)] for e in dir(instance) if '__' not in e and not hasattr(
+            getattr(instance, e), '__call__')])
+        proper = dict([[p, getattr(instance, e).__get__(instance, type(instance))]
+                       for p in full if hasattr(full[p], 'fset')])
         full.update(proper)
         return full
-    return typping(origin_class)
+    return typping(origin_instance)
